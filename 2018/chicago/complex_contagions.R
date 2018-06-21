481d3c543a98672f262694f0551b358c43106d0d
becker-likes-networks <jbecker@asc.upenn.edu>
2018-06-21 09:55:31 -0500

    better comments for complex contagions

--- 2018/chicago/complex_contagions.R
+++ 2018/chicago/complex_contagions.R
@@ -1,16 +1,28 @@
+#########################
+### SET EVERYTHING UP ###
+#########################
+
+
+### load library
 library(igraph)
 
+### create network to be used for model
 g = graph.lattice(dim=1, nei=2, length=20, circular=T)
 
-
-
-### setup fixed layout
+### setup fixed layout for consistent visualization
 layout = layout.auto(g)
 
 ### set starting conditions and plot
-V(g)$state = 0
-V(g)[1:5]$state = 1
+V(g)$state = 0        ### all states are zero
+V(g)[1:5]$state = 1   ### except node 3 and their neighbors
+
+### plot starting conditions
 plot(g, vertex.color=V(g)$state, layout=layout)
+
+
+######################
+#### RUN THE MODEL ###
+######################
 
 ### create holder variable for the new state
 V(g)$newstate=V(g)$state
@@ -25,4 +37,4 @@ for(i in 1:vcount(g)) {
 V(g)$state = V(g)$newstate
 
 ### plot again
-plot(g, vertex.color=V(g)$newstate, layout=layout)``
+plot(g, vertex.color=V(g)$newstate, layout=layout)
